8ddf045853a045e6dbbfeb1f5ce160cb8190dacb
teanocrata <teanocrata@gmail.com>
2016-04-19 19:01:03 +0200

    Work without pc - Funciona sin pc
    Work without pc connected and basic control error
    
    Funciona sin estar conectado al ordenador, control básico de errores

--- Arduino/openSmartCountrySensor/openSmartCountrySensor.ino
+++ Arduino/openSmartCountrySensor/openSmartCountrySensor.ino
@@ -3,8 +3,10 @@
 #include "aws_iot_config.h"
 
 aws_iot_mqtt_client myClient; //inicio el cliente mqtt
-char msg[100]; //bufer de lectura y escritura para el mensaje a enviar TODO pasarlo a un mensaje por cada sensor
+char msg[100]; //bufer de escritura para el mensaje JSON a enviar
 bool success_connect = false; //indicador para saber si está o no conectado
+int error_counter = 0;
+bool debug_mode = false;
 
 // Función de log
 bool print_log(char* src, int code) {
@@ -21,6 +23,7 @@ bool print_log(char* src, int code) {
     Serial.println(F(" code: "));
     Serial.print(code);
     ret = false;
+    error_counter++;
   }
   return ret;
 }
@@ -31,56 +34,25 @@ const int tempSensor = A1;
 
 //Variables para guardar el valor del sensor en cada intervalo de medida
 int lightValue = 0;
-int desiredLight = 0;
 float tempValue = 0.0;
-float desiredTemp = 0.0;
-char float_buf[5];
 
-
-// Función para sacar el dato de temperatura, sirve como delta para controlar los cambios entre el valor que tiene la sombra y el que se envía (¿Seguro?)
-void msg_callback_delta(char* src, int len) {
-  String data = String(src);
-
-  int st = data.indexOf("\"state\":") + strlen("\"state\":");
-  int ed = data.indexOf(",\"metadata\":");
-  String delta = data.substring(st, ed);
-
-  st = delta.indexOf("\"Temp\":") + strlen("\"Temp\":");
-  ed = delta.indexOf("\"Light\":");
-  String delta_temp_data = delta.substring(st, ed);
-  desiredTemp = delta_temp_data.toFloat();
-
-  st = delta.indexOf("\"Light\":") + strlen("\"Light\":");
-  ed = delta.indexOf("}");
-  String delta_light_data = delta.substring(st, ed);
-  desiredLight = delta_light_data.toInt();
-}
+// Función para sacar datos que cambien en el shadow, por ejemplo si se modifican desde una app, de momento no la usamos
+//void msg_callback_delta(char* src, int len) {
+//}
 
 void setup() {
   // Inicializa el Serial para la salida y espera hasta que está activo
   Serial.begin(115200);
-  while (!Serial);
+  if (debug_mode) {
+    while (!Serial);
+  }
 
   //Saca por el serial la versión del SDK de Amazon que estamos utilizando, los valores están en el archivo aws_iot_version.h en la carpeta de la librería
   char curr_version[80];
   sprintf(curr_version, "AWS IoT SDK Version(dev) %d.%d.%d-%s\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_TAG);
   Serial.println(curr_version);
 
-  while (!success_connect) {
-    //Configura el cliente
-    if (print_log("setup", myClient.setup(AWS_IOT_CLIENT_ID))) {
-      //Se carga la configuración de usuario que está especificada en el archivo aws_iot_config.h
-      if ((print_log("config", myClient.config(AWS_IOT_MQTT_HOST, AWS_IOT_MQTT_PORT, AWS_IOT_ROOT_CA_PATH, AWS_IOT_PRIVATE_KEY_PATH, AWS_IOT_CERTIFICATE_PATH)))) {
-        //Se intenta conectar con la configuración redeterminada: 60 segundos
-        if (print_log("connect", myClient.connect())) {
-          success_connect = true;
-          // Mediante la conexión, se inicializa el shadow y se registra la delta function
-          print_log("shadow init", myClient.shadow_init(AWS_IOT_MY_THING_NAME));
-          print_log("register thing shadow delta function", myClient.shadow_register_delta_func(AWS_IOT_MY_THING_NAME, msg_callback_delta));
-        }
-      }
-    }
-  }
+  conectTOAWSIoT();
 }
 
 void loop() {
@@ -92,17 +64,42 @@ void loop() {
 
   if (success_connect) {
 
-    dtostrf(tempValue, 4, 1, float_buf);
-    float_buf[4] = '\0';
-
     //Se construye el mensaje en formato JSON con los valores de temperatura y luz
-    sprintf(msg, "{\"state\":{\"reported\":{\"Temp\":%s, \"Light\":%d}}}", float_buf, lightValue);
+    sprintf(msg, "{\"state\":{\"reported\":{\"Temp\":%d, \"Light\":%d}}}", tempValue, lightValue);
 
-    //Se publica el mensaje en el tópico que se pasa por parámetro
+    //Se actualiza la shadow
     print_log("shadow update", myClient.shadow_update(AWS_IOT_MY_THING_NAME, msg, strlen(msg), NULL, 5));
 
     print_log("yield", myClient.yield());
 
     delay(1000);
+
+    if (error_counter >= 10) {
+      Serial.println(F("Too many failures: reset connection"));
+      success_connect = false;
+      //TODO de momento no hago el disconnect porque no funciona, en todo caso si él por las pruebas parece que no va mal la nueva conexión, lo que no se es si se estará acumulando algo que no deba
+      //print_log("disconnect", myClient.disconnect());
+      conectTOAWSIoT();
+    }
+  }
+}
+
+void conectTOAWSIoT() {
+  while (!success_connect) {
+    //Configura el cliente
+    if (print_log("setup", myClient.setup(AWS_IOT_CLIENT_ID))) {
+      //Se carga la configuración de usuario que está especificada en el archivo aws_iot_config.h
+      if ((print_log("config", myClient.config(AWS_IOT_MQTT_HOST, AWS_IOT_MQTT_PORT, AWS_IOT_ROOT_CA_PATH, AWS_IOT_PRIVATE_KEY_PATH, AWS_IOT_CERTIFICATE_PATH)))) {
+        //Se intenta conectar con la configuración redeterminada: 60 segundos
+        if (print_log("connect", myClient.connect())) {
+          success_connect = true;
+          error_counter = 0;
+          // Mediante la conexión, se inicializa el shadow y se registra la delta function
+          print_log("shadow init", myClient.shadow_init(AWS_IOT_MY_THING_NAME));
+          // De momento no se utiliza la parte de actuzlización desde un sitio externo, por eso se deja comentada esta parte
+          //print_log("register thing shadow delta function", myClient.shadow_register_delta_func(AWS_IOT_MY_THING_NAME, msg_callback_delta));
+        }
+      }
+    }
   }
 }
