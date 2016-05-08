b32e2531ad95d01141ea5e43f921f5e6565fc53e
teanocrata <teanocrata@gmail.com>
2016-05-08 13:17:07 +0200

    First version of GRPS configuration and https sending
    Primera versión que configura la sim, el gprs para una tarjeta de
    vodafone y realiza una petición de prueba para ver que está todo bien,
    necesita mucha refactorización y añadir toda la lógica de la información
    de los sensores

---
+++ Arduino/openSmartCountryGRPS/conf.h
@@ -0,0 +1,8 @@
+//Archivo de configuración
+char SIM_PIN[5] = "8651";   // your sim pin
+#define DEBUG_MODE true
+
+//Códigos de error
+#define CORRECTO 0 
+#define AT_ERROR 1  
+#define PIN_ERROR 2  
