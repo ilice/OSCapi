8cab4d62989bf95693138bfb56a67d9711f57e97
teanocrata <teanocrata@gmail.com>
2016-05-09 16:08:57 +0200

    New starting

--- Arduino/openSmartCountryGRPS/conf.h
+++
@@ -1,8 +0,0 @@
-//Archivo de configuración
-char SIM_PIN[5] = "8651";   // your sim pin
-#define DEBUG_MODE true
-
-//Códigos de error
-#define CORRECTO 0 
-#define AT_ERROR 1  
-#define PIN_ERROR 2  
