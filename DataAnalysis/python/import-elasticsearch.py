65a32d2912da9ebb37279c9416bea8c9bdb81db6
jlafuente <jlafuente@decidesoluciones.es>
2016-07-12 18:54:57 +0200

    parametrized script

--- DataAnalysis/python/import-elasticsearch.py
+++ DataAnalysis/python/import-elasticsearch.py
@@ -2,6 +2,7 @@ import os
 from elasticsearch_dsl.connections import connections
 import osc.importer.inforiego as inforiego
 import osc.util as util
+import sys
 
 connections.create_connection('default', hosts=['81.61.197.16:9200'], timeout=20)
 
@@ -13,14 +14,13 @@ encoding = 'iso-8859-1'
 
 inforiego.set_error_handler(util.ErrorHandler(err_dir))
 
-for y in ['2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006',
-          '2005', '2004', '2003', '2002', '2001']:
-    year = str(y)
+if __name__ == "__main__":
+    for y in sys.argv[1:]:
+        year = str(y)
 
-    print 'Importing ' + year
+        print 'Importing ' + year
 
-    inforiego.save2elasticsearch(years=[year],
-                                 data_dir=data_dir,
-                                 encoding=encoding,
-                                 tmp_dir=tmp_dir)
-
+        inforiego.save2elasticsearch(years=[year],
+                                     data_dir=data_dir,
+                                     encoding=encoding,
+                                     tmp_dir=tmp_dir)
