42df3d1a8ddef86ea55caa8e879861b390e7363f
teanocrata <teanocrata@gmail.com>
2016-07-14 11:21:35 +0200

    #50 fixed añadido número de páginas encontradas
    También se añade la caja de búsqueda por texto y ahora se usa el método
    post en lugar de get

--- Web/js/cultivos.js
+++ Web/js/cultivos.js
@@ -1,92 +1,123 @@
 var numeroCultivosPorPagina = 6;
 var ultimoCultivoCargado = 0;
 var paginaActual = 1;
+var elementosEncontrados = 0;
 
+function cargaDatos() {
 
-function cargaDatos(){
-	
-	var paginaInicial = true;
-	paginaMas(ultimoCultivoCargado,numeroCultivosPorPagina);
-	document.getElementById('pagina').innerHTML = ultimoCultivoCargado/numeroCultivosPorPagina;
-	
-	
+	ultimoCultivoCargado = 0;
+	paginaMas(ultimoCultivoCargado, numeroCultivosPorPagina);
+
 }
 
-function cargaCultivos(numeroCultivoInicial, numeroCultivosACargar){
-	var url = "https://search-opensmartcountry-trmalel6c5huhmpfhdh7j7m7ey.eu-west-1.es.amazonaws.com/osc/_search?size="+numeroCultivosACargar+"&from="+numeroCultivoInicial;
+function cargaCultivos(numeroCultivoInicial, numeroCultivosACargar) {
+
+	var textoBusqueda = QueryString.search;
+
+	var queryString = (encodeURIComponent(textoBusqueda) == "undefined") ? "" : ("q=" + encodeURIComponent(textoBusqueda) + "&");
+
+	var data = {
+		"from" : numeroCultivoInicial,
+		"size" : numeroCultivosACargar
+
+	};
+
+	var url = "https://search-opensmartcountry-trmalel6c5huhmpfhdh7j7m7ey.eu-west-1.es.amazonaws.com/osc/_search";
 
 	var request = jQuery.ajax({
 			crossDomain : true,
 			url : url,
-			method : "GET",
+			data : JSON.stringify(data),
+			type : 'POST',
 			dataType : "json"
 		});
-	
+
 	request.done(function (response, textStatus, jqXHR) {
 		var hits = response["hits"]["hits"];
 		document.getElementById('photoGrid').innerHTML = "";
-		for(var i=0; i<hits.length; i++){
+		for (var i = 0; i < hits.length; i++) {
 			var hit = hits[i];
 			var contenido = '<div onclick="document.getElementById(\'' + hit["_id"] + '_modal\').style.display=\'block\'">' +
-			'<div class="w3-third w3-container">' +
-			'<div id="'+ hit["_id"] +'" class="w3-margin w3-card-8 w3-hover-opacity">' + 
-			'<img src="img/cultivos/'+hit["_id"]+'.jpg" style="width:100%"/>' +
-			'<div class="w3-container">' +
-			'<h5><strong>' + hit["_source"]["Nombres Comunes"] + "</strong></h5>" +
-			'<h5 align="right"><em>' +hit["_source"]["Nombre Científico"] +'</em></h5>' +
-			'</div></div></div>' +
-			'</div>';
-			
-			
-			var modal = '<div id="'+ hit["_id"] +'_modal" class="w3-modal">' +
-			' <div class="w3-modal-content">' +
-			'<div class="w3-container">' +
-			'<span onclick="document.getElementById(\'' + hit["_id"] + '_modal\').style.display=\'none\'" ' +
-			'class="w3-closebtn">&times;</span>' +
-			'<table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">' +
-			'<tr><td>Origen</td><td>'+ hit["_source"]["Origen"] +'</td></tr>' +
-			'<tr><td>Distribución</td><td>'+ hit["_source"]["Distribución"] +'</td></tr>' +
-			'<tr><td>Adaptación</td><td>'+ hit["_source"]["Adaptación"] +'</td></tr>' +
-			'<tr><td>Ciclo vegetativo</td><td>'+ hit["_source"]["Ciclo vegetativo"] +'</td></tr>' +
-			'<tr><td>Tipo Fotosintético</td><td>'+ hit["_source"]["Tipo Fotosintético"] +'</td></tr>' +
-			'<tr><td>Fotoperíodo</td><td>'+ hit["_source"]["Fotoperíodo"] +'</td></tr>' +
-			'<tr><td>Altitud</td><td>'+ hit["_source"]["Altitud"] +'</td></tr>' +
-			'<tr><td>Precipitación</td><td>'+ hit["_source"]["Precipitación"] +'</td></tr>' +
-			'<tr><td>Humedad ambiental</td><td>'+ hit["_source"]["Humedad ambiental"] +'</td></tr>' +
-			'<tr><td>Temperatura</td><td>'+ hit["_source"]["Temperatura"] +'</td></tr>' +
-			'<tr><td>Luz</td><td>'+ hit["_source"]["Luz"] +'</td></tr>' +
-			'<tr><td>Textura de suelo</td><td>'+ hit["_source"]["Textura de suelo"] +'</td></tr>' +
-			'<tr><td>Profundidad de suelo</td><td>'+ hit["_source"]["Profundidad de suelo"] +'</td></tr>' +
-			'<tr><td>Salinidad</td><td>'+ hit["_source"]["Salinidad"] +'</td></tr>' +
-			'<tr><td>pH</td><td>'+ hit["_source"]["pH"] +'</td></tr>' +
-			'<tr><td>Drenaje</td><td>'+ hit["_source"]["Drenaje"] +'</td></tr>' +
-			'<tr><td>Otros</td><td>'+ hit["_source"]["Otros"] +'</td></tr>' +
-			'</table>' +
-			'</div>' +
-			'</div>' +
-			'</div>';
-			
+				'<div class="w3-third w3-container">' +
+				'<div id="' + hit["_id"] + '" class="w3-margin w3-card-8 w3-hover-opacity">' +
+				'<img src="img/cultivos/' + hit["_id"] + '.jpg" style="width:100%"/>' +
+				'<div class="w3-container">' +
+				'<h4>' + hit["_source"]["Nombres Comunes"] + "</h4>" +
+				'<h5 align="right"><em>' + hit["_source"]["Nombre Científico"] + '</em></h5>' +
+				'</div></div></div>' +
+				'</div>';
 
+			var modal = '<div id="' + hit["_id"] + '_modal" class="w3-modal">' +
+				' <div class="w3-modal-content">' +
+				'<div class="w3-container">' +
+				'<span onclick="document.getElementById(\'' + hit["_id"] + '_modal\').style.display=\'none\'" ' +
+				'class="w3-closebtn">&times;</span>' +
+				'<table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">' +
+				'<tr><td>Origen</td><td>' + hit["_source"]["Origen"] + '</td></tr>' +
+				'<tr><td>Distribución</td><td>' + hit["_source"]["Distribución"] + '</td></tr>' +
+				'<tr><td>Adaptación</td><td>' + hit["_source"]["Adaptación"] + '</td></tr>' +
+				'<tr><td>Ciclo vegetativo</td><td>' + hit["_source"]["Ciclo vegetativo"] + '</td></tr>' +
+				'<tr><td>Tipo Fotosintético</td><td>' + hit["_source"]["Tipo Fotosintético"] + '</td></tr>' +
+				'<tr><td>Fotoperíodo</td><td>' + hit["_source"]["Fotoperíodo"] + '</td></tr>' +
+				'<tr><td>Altitud</td><td>' + hit["_source"]["Altitud"] + '</td></tr>' +
+				'<tr><td>Precipitación</td><td>' + hit["_source"]["Precipitación"] + '</td></tr>' +
+				'<tr><td>Humedad ambiental</td><td>' + hit["_source"]["Humedad ambiental"] + '</td></tr>' +
+				'<tr><td>Temperatura</td><td>' + hit["_source"]["Temperatura"] + '</td></tr>' +
+				'<tr><td>Luz</td><td>' + hit["_source"]["Luz"] + '</td></tr>' +
+				'<tr><td>Textura de suelo</td><td>' + hit["_source"]["Textura de suelo"] + '</td></tr>' +
+				'<tr><td>Profundidad de suelo</td><td>' + hit["_source"]["Profundidad de suelo"] + '</td></tr>' +
+				'<tr><td>Salinidad</td><td>' + hit["_source"]["Salinidad"] + '</td></tr>' +
+				'<tr><td>pH</td><td>' + hit["_source"]["pH"] + '</td></tr>' +
+				'<tr><td>Drenaje</td><td>' + hit["_source"]["Drenaje"] + '</td></tr>' +
+				'<tr><td>Otros</td><td>' + hit["_source"]["Otros"] + '</td></tr>' +
+				'</table>' +
+				'</div>' +
+				'</div>' +
+				'</div>';
 
 			document.getElementById('photoGrid').innerHTML += contenido;
 			document.getElementById('photoGrid').innerHTML += modal;
-			
+
 		}
+		elementosEncontrados = response["hits"].total;
 		ultimoCultivoCargado = numeroCultivoInicial + numeroCultivosACargar;
+		document.getElementById('pagina').innerHTML = (ultimoCultivoCargado / numeroCultivosPorPagina) + " / " + Math.ceil((elementosEncontrados / numeroCultivosPorPagina));
 	});
-}
-
-function paginaMas(){
-	cargaCultivos(ultimoCultivoCargado, numeroCultivosPorPagina);
-	document.getElementById('pagina').innerHTML = ultimoCultivoCargado/numeroCultivosPorPagina;
-}
-
-function paginaMenos(){
-	cargaCultivos(ultimoCultivoCargado-2*numeroCultivosPorPagina, numeroCultivosPorPagina);
-	document.getElementById('pagina').innerHTML = ultimoCultivoCargado/numeroCultivosPorPagina;
-}
-
-function pagina(numeroPagina){
-	cargaCultivos(numeroPagina*numeroCultivosPorPagina, numeroCultivosPorPagina);
+	
 	
 }
+
+function paginaMas() {
+	cargaCultivos(ultimoCultivoCargado, numeroCultivosPorPagina);
+	
+}
+
+function paginaMenos() {
+	cargaCultivos(ultimoCultivoCargado - 2 * numeroCultivosPorPagina, numeroCultivosPorPagina);
+}
+
+var QueryString = function () {
+	// This function is anonymous, is executed immediately and
+	// the return value is assigned to QueryString!
+	var query_string = {};
+	var query = window.location.search.substring(1);
+	if (query.length > 0) {
+		var vars = query.split("&");
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split("=");
+			// If first entry with this name
+			if (typeof query_string[pair[0]] === "undefined") {
+				query_string[pair[0]] = decodeURIComponent(pair[1]);
+				// If second entry with this name
+			} else if (typeof query_string[pair[0]] === "string") {
+				var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+				query_string[pair[0]] = arr;
+				// If third or later entry with this name
+			} else {
+				query_string[pair[0]].push(decodeURIComponent(pair[1]));
+			}
+		}
+	}
+	return query_string;
+}
+();
