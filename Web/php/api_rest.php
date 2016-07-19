3864212b3900e72806f6b2a93c6e0dd566edbf93
teanocrata <teanocrata@gmail.com>
2016-07-19 17:20:11 +0200

    #52 un error en el type

--- Web/php/api_rest.php
+++ Web/php/api_rest.php
@@ -11,17 +11,33 @@ $input = file_get_contents('php://input');
 
 // retrieve the index and type from the path
 $index = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
-$type = array_shift($request)+0;
+error_log("index:$index");
+$requeststring = json_encode($request);
+error_log("request:$requeststring");
+$type = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
 error_log("type:$type",0);
 
 $data = json_encode($_POST);
 
 
-$url = "https://search-opensmartcountry-trmalel6c5huhmpfhdh7j7m7ey.eu-west-1.es.amazonaws.com/$index/_search";
+$url = "https://search-opensmartcountry-trmalel6c5huhmpfhdh7j7m7ey.eu-west-1.es.amazonaws.com";
+
+if(strlen($index)>0){
+	$url = "$url/$index" ;
+	
+	if(strlen($type)>0){
+		$url = "$url/$type" ;
+	}
+}
+
+
+
+$url = "$url/_search" ;
 
 if(strlen($querystring)>0){
 	$url = "$url?$querystring" ;
 }
+
 $handler = curl_init($url);  
 
 if($method=="POST"){
