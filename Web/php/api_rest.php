1f2c754e2f0b8d7f4896fca2982fdc80f3fc8164
teanocrata <teanocrata@gmail.com>
2016-07-31 12:02:47 +0200

    Revert "Fixed #59"
    This reverts commit cc710a4699c54e9dd09980e3c479ab6df360cfc3.

--- Web/php/api_rest.php
+++ Web/php/api_rest.php
@@ -17,7 +17,7 @@ $type = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
 $data = json_encode($_POST);
 
 
-$url = "http://81.61.197.16:9200";
+$url = "https://search-opensmartcountry-trmalel6c5huhmpfhdh7j7m7ey.eu-west-1.es.amazonaws.com";
 
 if(strlen($index)>0){
 	$url = "$url/$index" ;
