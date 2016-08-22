a322e6f8c22e679e925f46245e528144748109c1
teanocrata <teanocrata@gmail.com>
2016-08-23 00:15:29 +0200

    #13 Testing Travis CI

--- Web/php/test/all_test.php
+++ Web/php/test/all_test.php
@@ -2,7 +2,7 @@
 ini_set("display_errors", "1");
 error_reporting(E_ALL);
 
-require_once(dirname(__FILE__) . '/simpletest/autorun.php');
+require_once(dirname(__FILE__) . 'vendor/simpletest/simpletest/autorun.php');
 
 class AllTests extends TestSuite {
     function __construct() {
