725d6fa10d10235d9bb6644f8a9e760c11a01aa0
teanocrata <teanocrata@gmail.com>
2016-08-23 16:46:51 +0200

    #13 Travis CI ejecuta todas las pruebas

--- Web/php/test/all_test.php
+++ Web/php/test/all_test.php
@@ -7,8 +7,8 @@ require_once( __DIR__ . '/../../../vendor/simpletest/simpletest/autorun.php');
 class AllTests extends TestSuite {
     function __construct() {
         parent::__construct();
-        $this->addFile('altitud_test.php');
-        $this->addFile('api_rest_test.php');
+        $this->addFile(dirname(__FILE__) . '\altitud_test.php');
+        $this->addFile(dirname(__FILE__) . '\api_rest_test.php');
     }
 }
 ?>
