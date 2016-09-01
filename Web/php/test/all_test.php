f6a66a7195adcf3465f09a81878f4642d16c3dbe
teanocrata <teanocrata@gmail.com>
2016-09-01 07:59:06 +0200

    Faltaban los test del catastro

--- Web/php/test/all_test.php
+++ Web/php/test/all_test.php
@@ -9,6 +9,7 @@ class AllTests extends TestSuite {
         parent::__construct();
         $this->addFile(dirname(__FILE__) . '/altitud_test.php');
         $this->addFile(dirname(__FILE__) . '/api_rest_test.php');
+        $this->addFile(dirname(__FILE__) . '/catastro_test.php');
     }
 }
 ?>
