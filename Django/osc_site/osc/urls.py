866c57c82c54f311c836a23be1e5132eac4fa76f
jlafuente <paquito0311>
2016-09-17 00:04:51 +0200

    Acabada la parte web, faltan los servicios

--- Django/osc_site/osc/urls.py
+++ Django/osc_site/osc/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^cultivos/$', views.cultivos, name='cultivos'),
+    url(r'^parcela/$', views.parcela, name='parcela'),
+    url(r'^oscar/$', views.oscar, name='oscar'),
+    url(r'^mapa_de_parcelas/$', views.mapa_de_parcelas, name='mapa_de_parcelas'),
 ]
--- Django/osc_site/osc/views.py
+++ Django/osc_site/osc/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'osc/index.html', {})
+
+
+def cultivos(request):
+    return render(request, 'osc/cultivos.html', {})
+
+
+def parcela(request):
+    return render(request, 'osc/parcela.html', {})
+
+
+def oscar(request):
+    return render(request, 'osc/oscar.html', {})
+
+
+def mapa_de_parcelas(request):
+    return render(request, 'osc/mapaDeParcelas.html', {})
