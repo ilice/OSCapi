5b256940163eec9c720f9f16a7179c95f742285f
teanocrata <teanocrata@gmail.com>
2016-10-24 14:51:34 +0200

    #126 Apuntando todo a la nueva máquina

--- Web/php/config.php
+++ Web/php/config.php
@@ -1,11 +1,11 @@
 <?php
 return array(
-		'elasticendpoint' => 'http://82.158.80.73:9200',
+		'elasticendpoint' => 'http://94.76.229.213:9200',
 		'agroclimaticIndex' => "inforiego",
 		'agroclimaticStationType' => "info_riego_station",
 		'googleMapsElevationEndpoint' => 'https://maps.googleapis.com/maps/api/elevation/json?key=AIzaSyB-K-4XmS9a5ItnkrqJSS9070qAeRuXt6M',
 		'catastroOVCSWLocalizacionRCendpoint' => 'http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/',
 		'slackHookEndpoint' => "https://hooks.slack.com/services/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",
-		'quijoteServerEndPoint' => "http://89.141.96.206:8000"
+		'quijoteServerEndPoint' => "http://94.76.229.213:8000"
 );
 ?>
