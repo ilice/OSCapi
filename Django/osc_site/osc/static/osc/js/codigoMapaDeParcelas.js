37a68641a7f7bb7378848aca8ab10c7d8df7529c
teanocrata <teanocrata@gmail.com>
2016-11-03 18:43:56 +0100

    Fixed #144
    Needs to be refactored

--- Django/osc_site/osc/static/osc/js/codigoMapaDeParcelas.js
+++ Django/osc_site/osc/static/osc/js/codigoMapaDeParcelas.js
@@ -57,8 +57,14 @@ function inicializaMapa() {
 	var input = /** @type {!HTMLInputElement} */
 	(document.getElementById('buscador'));
 
-	var autocomplete = new google.maps.places.Autocomplete(input);
+    var autocomplete = new google.maps.places.Autocomplete(input);
 	autocomplete.bindTo('bounds', mapa);
+
+	var inputSmall = /** @type {!HTMLInputElement} */
+	(document.getElementById('buscadorSmall'));
+
+	var autocompleteSmall = new google.maps.places.Autocomplete(inputSmall);
+	autocompleteSmall.bindTo('bounds', mapa);
 
 	var infowindow = new google.maps.InfoWindow();
 	var marker = new google.maps.Marker({
@@ -72,10 +78,60 @@ function inicializaMapa() {
 		geocodeAddress(geocoder, mapa);
 	});
 
+	document.getElementById('buscarSmall').addEventListener('click', function() {
+		geocodeAddress(geocoder, mapa);
+	});
+
 	autocomplete.addListener('place_changed', function() {
 		infowindow.close();
 		marker.setVisible(false);
 		var place = autocomplete.getPlace();
+		if (!place.geometry) {
+			geocodeAddress(geocoder, mapa);
+			return;
+		}
+
+		// If the place has a geometry, then present it on a
+		// map.
+		if (place.geometry.viewport) {
+			mapa.fitBounds(place.geometry.viewport);
+		} else {
+			mapa.setCenter(place.geometry.location);
+			mapa.setZoom(17); // Why 17? Because it looks
+			// good.
+		}
+		marker.setIcon(/** @type {google.maps.Icon} */
+		({
+			url : place.icon,
+			size : new google.maps.Size(71, 71),
+			origin : new google.maps.Point(0, 0),
+			anchor : new google.maps.Point(17, 34),
+			scaledSize : new google.maps.Size(35, 35)
+		}));
+		marker.setPosition(place.geometry.location);
+		// marker.setVisible(true);
+
+		var address = '';
+		if (place.address_components) {
+			address = [
+					(place.address_components[0]
+							&& place.address_components[0].short_name || ''),
+					(place.address_components[1]
+							&& place.address_components[1].short_name || ''),
+					(place.address_components[2]
+							&& place.address_components[2].short_name || '') ]
+					.join(' ');
+		}
+
+		infowindow.setContent('<div><strong>' + place.name + '</strong><br>'
+				+ address);
+		infowindow.open(mapa, marker);
+	});
+
+    autocompleteSmall.addListener('place_changed', function() {
+		infowindow.close();
+		marker.setVisible(false);
+		var place = autocompleteSmall.getPlace();
 		if (!place.geometry) {
 			geocodeAddress(geocoder, mapa);
 			return;
@@ -269,6 +325,10 @@ function limpiaMarcadores() {
 
 function geocodeAddress(geocoder, resultsMap) {
 	var address = document.getElementById('buscador').value;
+	var addressSmall = document.getElementById('buscadorSmall').value;
+	if(addressSmall != "" && address == ""){
+	    address = addressSmall;
+	}
 	geocoder
 			.geocode(
 					{
