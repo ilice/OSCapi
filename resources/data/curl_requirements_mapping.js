2f387eeb86478679034f1acfdbf3090ff4666c7d
teanocrata <teanocrata@gmail.com>
2016-11-05 14:23:22 +0100

    ph data and requirements mapping

---
+++ resources/data/curl_requirements_mapping.js
@@ -0,0 +1,168 @@
+{
+	"mappings" : {
+		"requirements" : {
+			"properties" : {
+				"Adaptación" : {
+					"type" : "string"
+				},
+				"Altitud" : {
+					"type" : "string"
+				},
+				"Ciclo vegetativo" : {
+					"type" : "string"
+				},
+				"Clave" : {
+					"type" : "string"
+				},
+				"Distribución" : {
+					"type" : "string"
+				},
+				"Drenaje" : {
+					"type" : "string"
+				},
+				"Familia" : {
+					"type" : "string"
+				},
+				"Foto" : {
+					"type" : "string"
+				},
+				"Fotoperíodo" : {
+					"type" : "string"
+				},
+				"Humedad ambiental" : {
+					"type" : "string"
+				},
+				"Luz" : {
+					"type" : "string"
+				},
+				"Nombre" : {
+					"type" : "string"
+				},
+				"Nombre Científico" : {
+					"type" : "string"
+				},
+				"Nombres Comunes" : {
+					"type" : "string"
+				},
+				"Origen" : {
+					"type" : "string"
+				},
+				"Otros" : {
+					"type" : "string"
+				},
+				"Precipitación" : {
+					"type" : "string"
+				},
+				"Profundidad de suelo" : {
+					"type" : "string"
+				},
+				"Salinidad" : {
+					"type" : "string"
+				},
+				"Temperatura" : {
+					"type" : "string"
+				},
+				"Textura de suelo" : {
+					"type" : "string"
+				},
+				"Tipo Fotosintético" : {
+					"type" : "string"
+				},
+				"altitude" : {
+					"properties" : {
+						"max" : {
+							"type" : "integer"
+						},
+						"min" : {
+							"type" : "integer"
+						},
+						"obs" : {
+							"type" : "string"
+						},
+						"source" : {
+							"properties" : {
+								"name" : {
+									"type" : "string"
+								},
+								"year" : {
+									"type" : "integer"
+								}
+							}
+						}
+					}
+				},
+				"labels" : {
+					"type" : "string"
+				},
+				"pH" : {
+					"type" : "string"
+				},
+				"ph" : {
+					"properties" : {
+						"max" : {
+							"type" : "float"
+						},
+						"min" : {
+							"type" : "float"
+						},
+						"obs" : {
+							"type" : "string"
+						}
+					}
+				},
+				"rainfall" : {
+					"properties" : {
+						"max" : {
+							"type" : "float"
+						},
+						"min" : {
+							"type" : "float"
+						},
+						"obs" : {
+							"type" : "string"
+						}
+					}
+				},
+				"sunHours" : {
+					"properties" : {
+						"max" : {
+							"type" : "integer"
+						},
+						"min" : {
+							"type" : "integer"
+						},
+						"obs" : {
+							"type" : "string"
+						}
+					}
+				},
+				"temperature" : {
+					"properties" : {
+						"max" : {
+							"type" : "float"
+						},
+						"min" : {
+							"type" : "float"
+						},
+						"obs" : {
+							"type" : "string"
+						}
+					}
+				},
+				"latitude" : {
+					"properties" : {
+						"max" : {
+							"type" : "integer"
+						},
+						"min" : {
+							"type" : "integer"
+						},
+						"obs" : {
+							"type" : "string"
+						}
+					}
+				}
+			}
+		}
+	}
+}
