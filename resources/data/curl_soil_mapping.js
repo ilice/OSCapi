7aa102202f01eb92aaee0f7ddc1e2f47b221e26a
teanocrata <teanocrata@gmail.com>
2016-11-18 10:33:43 +0100

    #146 #148 Errors in data

---
+++ resources/data/curl_soil_mapping.js
@@ -0,0 +1,53 @@
+{
+   "mappings": {
+      "soil": {
+         "properties": {
+            "CEC": {
+               "type": "long"
+            },
+            "CaCO3": {
+               "type": "long"
+            },
+            "K": {
+               "type": "long"
+            },
+            "N": {
+               "type": "long"
+            },
+            "OC": {
+               "type": "long"
+            },
+            "P": {
+               "type": "long"
+            },
+            "POINT_ID": {
+               "type": "keyword"
+            },
+            "clay": {
+               "type": "long"
+            },
+            "coarse": {
+               "type": "long"
+            },
+            "coordinates": {
+               "type": "geo_point"
+            },
+            "pH_in_CaCl2": {
+               "type": "long"
+            },
+            "pH_in_H2O": {
+               "type": "long"
+            },
+            "sample_ID": {
+               "type": "long"
+            },
+            "sand": {
+               "type": "long"
+            },
+            "silt": {
+               "type": "long"
+            }
+         }
+      }
+   }
+}
