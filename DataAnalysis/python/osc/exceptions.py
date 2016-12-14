fdddc3b0973266cf05eb21974450e6daa143f736
teanocrata <teanocrata@gmail.com>
2016-12-14 13:34:44 +0100

    Splitted DataAnalysis  out into a new repository

--- DataAnalysis/python/osc/exceptions.py
+++
@@ -1,7 +0,0 @@
-class ConnectionError(Exception):
-    def __init__(self, service, message):
-        self.service = service
-        self.message = message
-
-    def __str__(self):
-        return 'ConnectionError (' + self.service + '): ' + self.message
