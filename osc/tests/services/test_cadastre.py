1dd7b26b1e251419432f52cd59d00e4d7fc14e2c
teanocrata <teanocrata@gmail.com>
2017-05-15 18:46:21 +0200

    Removed print from test

--- osc/tests/services/test_cadastre.py
+++ osc/tests/services/test_cadastre.py
@@ -13,7 +13,6 @@ class CadastreServiceTest(TestCase):
 
     def test_get_inspire_data(self):
         parcels = cadastre.get_inspire_data_by_code('11015A01400009')
-        print str(parcels)
 
     def test_public_cadastre_info(self):
         cadastre.get_public_cadastre_info('40167A00805001')
