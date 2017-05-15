725d9fd8996ce723bbd314368724e1d7cbf6424e
teanocrata <teanocrata@gmail.com>
2017-05-15 12:27:52 +0200

    Skipped elastic connection test

--- osc/tests/test_cadastre.py
+++ osc/tests/test_cadastre.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-from django.test import TestCase
+from django.test import TestCase, tag
 from unittest import skip
 
 import osc.importer.cadastre as impcadastre
@@ -11,16 +11,20 @@ class CadastreServiceTest(TestCase):
     def setUp(self):
         pass
 
+    @tag('elastic_connection')
     def test_get_inspire_data(self):
         parcels = cadastre.get_inspire_data_by_code('11015A01400009')
         print str(parcels)
 
+    @tag('elastic_connection')
     def test_public_cadastre_info(self):
         cadastre.get_public_cadastre_info('40167A00805001')
 
     @skip("Very very long test")
+    @tag('elastic_connection')
     def test_update_cadastral_information(self):
         impcadastre.update_cadastral_information()
 
+    @tag('elastic_connection')
     def test_create_mapping(self):
         cadastre.create_parcel_mapping()
