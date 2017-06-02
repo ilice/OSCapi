b7d57283aa668fe4491b80898467fd82df8bdce9
teanocrata <teanocrata@gmail.com>
2017-06-02 07:23:05 +0200

    #203 Test call update cadastre information when update parcels

--- osc/tests/management/update_parcels_test.py
+++ osc/tests/management/update_parcels_test.py
@@ -1,18 +1,13 @@
 from django.core.management import call_command
 from django.test import TestCase
 # from django.core.management.base import CommandError
-from unittest import skip
 import mock
 
 
 class UpdateParcelsTest(TestCase):
-    @mock.patch('osc.importer.cadastre.update_cadastre_information')
-    def test_fails_when_there_is_no_parcel_args(
-            self,
-            mock_import_cadastral_information):
-            call_command('update_parcels')
 
-    @mock.patch('osc.services.parcels.update_parcel_by_cadastral_code')
+    @mock.patch('osc.management.commands.update_parcels.'
+                'update_parcel_by_cadastral_code')
     def test_call_update_parcel_by_catastral_code(
             self,
             mock_update_parcel_by_cadastral_code):
@@ -20,6 +15,10 @@ class UpdateParcelsTest(TestCase):
         call_command('update_parcels', '--parcel=%s' % cadastral_code)
         mock_update_parcel_by_cadastral_code.assert_called_with(cadastral_code)
 
-    @skip
-    def test_update_parcel(self):
-        raise NotImplementedError("Update parcel")
+    @mock.patch('osc.management.commands.update_parcels.'
+                'update_cadastre_information')
+    def test_update_parcels(
+            self,
+            mock_update_cadastre_information):
+            call_command('update_parcels')
+            mock_update_cadastre_information.assert_called()
