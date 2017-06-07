1b66f48ca324647eeb8c75f3e7bfca8e6df5b927
teanocrata <teanocrata@gmail.com>
2017-06-07 07:26:49 +0200

    PEP8

--- osc/serializers.py
+++ osc/serializers.py
@@ -1,14 +1,15 @@
+from osc.models import UserParcel
+from osc.models import UserProfile
 from rest_framework import serializers
-from osc.models import UserProfile, UserParcel
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
+    class Meta(object):
         model = UserProfile
         fields = ('user',)
 
 
 class UserParcelSerializer(serializers.ModelSerializer):
-    class Meta:
+    class Meta(object):
         model = UserParcel
         fields = ('user', 'cadastral_code')
