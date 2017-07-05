1a10749f2b4ca3e6017e300089bab8d7c4fb4cd2
teanocrata <teanocrata@gmail.com>
2017-07-05 19:13:03 +0200

    skip takes 1 argument

--- osc/tests/importer/sigpac_test.py
+++ osc/tests/importer/sigpac_test.py
@@ -10,12 +10,12 @@ import osc.importer.sigpac as sigpac
 
 
 class SigpacImporterTest(TestCase):
-    @skip()
+    @skip('travis error')
     @override_settings(ERROR_HANDLER=['DBErrorHandler'])
     def test_updateParcel_raises_exception_when_no_params(self):
         self.assertRaises(ItacylException, sigpac.updateParcel, [])
 
-    @skip()
+    @skip('travis error')
     @override_settings(ERROR_HANDLER=['DBErrorHandler'])
     def test_updateParcel_raises_exception_when_bad_params(self):
         self.assertRaises(ItacylException,
@@ -31,7 +31,7 @@ class SigpacImporterTest(TestCase):
                           [410991618, 8274.60336, 368.00616,
                            5.1, 1, 0, 0, 507, 435, 1, 0, 'TA'])
 
-    @skip()
+    @skip('travis error')
     @mock.patch('osc.importer.sigpac.ITACYL_PROTOCOL', 'file:///')
     @mock.patch('osc.importer.sigpac.ITACYL_FTP',
                 'D:/teanocrata/Development/OSCapi/osc/tests/importer/fixtures')
@@ -41,7 +41,7 @@ class SigpacImporterTest(TestCase):
         sigpac.updateMunicipality(province, municipality)
         self.assertTrue(True, "shouldn't throw exceptions")
 
-    @skip()
+    @skip('travis error')
     @mock.patch('osc.importer.sigpac.ITACYL_PROTOCOL', 'file:///')
     @mock.patch('osc.importer.sigpac.ITACYL_FTP',
                 'D:/teanocrata/Development/OSCapi/osc/tests/importer/fixtures')
@@ -51,7 +51,7 @@ class SigpacImporterTest(TestCase):
         sigpac.updateMunicipality(province, municipality)
         self.assertTrue(True, "shouldn't throw exceptions")
 
-    @skip()
+    @skip('travis error')
     @mock.patch('osc.importer.sigpac.ITACYL_PROTOCOL', 'file:///')
     @mock.patch('osc.importer.sigpac.ITACYL_FTP',
                 'D:/teanocrata/Development/OSCapi/osc/tests/importer/fixtures')
@@ -66,7 +66,7 @@ class SigpacImporterTest(TestCase):
         m_updateParcel.assert_called_with(
             [895882, 121.32291, 50.39169, 37, 284, 0, 0, 1, 4, 2, 0, 'IM'])
 
-    @skip()
+    @skip('travis error')
     @mock.patch('osc.importer.sigpac.ITACYL_PROTOCOL', 'file:///')
     @mock.patch('osc.importer.sigpac.ITACYL_FTP',
                 'D:/teanocrata/Development/OSCapi/osc/tests/importer/fixtures')
