bb20d602bf20614cc6f55c602b4e0d79d173b22a
teanocrata <teanocrata@gmail.com>
2017-07-07 20:39:57 +0200

    PEP8 one import per line in import_sigpac_data

--- osc/management/commands/import_sigpac_data.py
+++ osc/management/commands/import_sigpac_data.py
@@ -1,4 +1,5 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
 
 from osc.importer.sigpac import import_sigpac_data
 
