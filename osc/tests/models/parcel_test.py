fb2523d06553efa8957d332d6b920aa300d827bc
teanocrata <teanocrata@gmail.com>
2017-08-22 18:45:07 +0200

    #208 Added test for parcels by bbox

--- osc/tests/models/parcel_test.py
+++ osc/tests/models/parcel_test.py
@@ -28,6 +28,41 @@ class ParcelTest(TestCase):
         getParcelByNationalCadastralReference(nationalCadastralReference='37284A00600106')
         mock_es.search.assert_called_once()
 
+    @mock.patch('osc.models.parcel.es')
+    def test_getParcel_calls_elastic_once(self, mock_es):
+        mock_es.search.return_value = self.get_parcel_by_bbox_response
+        getParcels(bbox='40.435861,-5.763941,40.441145,-5.746592')
+        mock_es.search.assert_called_with(
+            body={
+                'query':
+                {
+                    'bool':
+                    {
+                        'filter':
+                        {
+                            'geo_bounding_box':
+                            {
+                                'properties.reference_point':
+                                {'top': '40.441145', 'bottom': '40.435861', 'right': '-5.763941', 'left': '-5.746592'}
+                            }
+                        },
+                        'must': [
+                            {'exists': {'field': 'properties.sigpacData'}},
+                            {'exists': {'field': 'properties.elevation'}}
+                        ]
+                    }
+                }
+            },
+            doc_type='parcel',
+            index='parcels',
+            size=5000)
+
+    @mock.patch('osc.models.parcel.es')
+    def test_getParcel_with_bbox_info_calls_elastic_once_with_bounds(self, mock_es):
+        mock_es.search.return_value = self.get_parcel_by_bbox_response
+        getParcels()
+        mock_es.search.assert_called_once()
+
     @attr('elastic_connection')
     def test_getParcels(self):
         logger.debug(getParcels())
